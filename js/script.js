78855ece1a227b57e1794b1c66d2a37e1aca9ebc
JohnKOnyango <johnonyango399@gmail.com>
2022-03-20 22:41:40 +0300

    added a constractor

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ function topping() {
     var pizzaTopping = document.getElementById("top").value;
     return parseInt(pizzaFlavour);
 }
+function size() {
+    var pizzaSize = document.getElementById("form").value;
+    return parseInt(pizzaSize);
+}
+function number() {
+    var pizzaNumber = document.getElementById("quant").value;
+    return parseInt(pizzaNumber);
+}
+function Order(flavour, crust, topping, size, quant){
+    this.newCrust = crust;
+    this.newTopping = topping;
+    this.newQuant = quant;
+    this.newFlavour = flavour;
+}
