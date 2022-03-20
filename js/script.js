45615faf21b01093eedda3b90c1f34f1a3241edf
JohnKOnyango <johnonyango399@gmail.com>
2022-03-20 22:19:04 +0300

    added function for user's input

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function flavour() {
+    var pizzaFlavour = document.getElementById("flavour").value;
+    return parseInt(pizzaFlavour);
+}
+function crust() {
+    var pizzaCrust = document.getElementById("crust").value;
+    return parseInt(pizzaCrust);
+}
+function topping() {
+    var pizzaTopping = document.getElementById("top").value;
+    return parseInt(pizzaFlavour);
+}
